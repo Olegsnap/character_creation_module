5fe6b70e0eaa915f26865a889d9b303fb643d2ec
Олег Асташкин <snap710@yandex.ru>
2022-11-16 16:26:16 +0300

    Docstrings addeed

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+"""Модуль игры. Ввод имени, выбор класса и тренировка."""
 from random import randint
+
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Возвращает результат атаки."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -9,12 +13,14 @@ def attack(char_name: str, char_class: str) -> str:
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(5, 10)}')
     if char_class == 'healer':
-        return (f'{char_name} нанёс урон противнику равный '
+        return (f'{char_name} нанёс урон '
+                f' противнику равный '
                 f'{5 + randint(-3, -1)}')
     return (f'{char_name} не применил специальное умение')
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Возвращает результат защиты."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +31,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Возвращает результат спец.умения."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -38,6 +45,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Описывает выбор. Вызывает ввод команд."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -62,6 +70,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Вызывыет ввод имени. Предлагает выбрать класс персонажа."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -85,7 +94,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> str:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -95,6 +105,3 @@ def main() -> str:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
